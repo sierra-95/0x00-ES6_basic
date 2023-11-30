10b82f21f3039ed299cd79a64d17befe59b20922
Michael Machohi <111045570+sierra-95@users.noreply.github.com>
2023-11-30 17:27:14 +0300

    Create 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  /**
+   * Returns the values in the following format: name (code)
+   */
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
