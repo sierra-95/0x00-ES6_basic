f0d50716b53ed4fcab96529cae337595d68c59fe
Michael Machohi <111045570+sierra-95@users.noreply.github.com>
2023-12-11 09:33:46 +0300

    Create 9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * returns a map of groceries
+ */
+export default function groceriesList() {
+  const newMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return newMap;
+}
